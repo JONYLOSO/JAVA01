fddd48477f5e6482d44dfd033b52940bd196a217
JONYLOSO <tana_5678@hotmail.com>
2019-12-30 21:52:09 +0700

    test_2

--- n001.java
+++ n001.java
@@ -1,10 +1,23 @@
+import java.util.*;
 class n001{
 
     public static void main(String[] args){
 
+        char[] n = new char[10];
+        n[0]='n';  
 
-        System.out.println("AAAA");
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.clear();
+        System.out.println(cars.size());
+        System.out.println(cars.get(0));
+        cars.set(0, "AAAAAAAAAAA");
+        for(String p :cars)System.out.println(p);
 
+            
 
 
 
