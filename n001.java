3ca2b77d4cfd98c38b07ae81e6d1988a50cf0f65
JONYLOSO <tana_5678@hotmail.com>
2019-12-30 22:04:01 +0700

    test5

--- n001.java
+++ n001.java
@@ -2,7 +2,7 @@ import java.util.*;
 class n001{
 
     public static void main(String[] args){
-
+        Scanner sc=new Scanner(System.in);
         char[] n = new char[10];
         n[0]='n';  
 
@@ -11,12 +11,11 @@ class n001{
         cars.add("BMW");
         cars.add("Ford");
         cars.add("Mazda");
-        cars.clear();
-        System.out.println(cars.size());
+        cars.add(sc.nextLine());
+       // cars.clear();
+        System.out.println("===========================\n"+cars.size());
         System.out.println(cars.get(0));
         cars.set(0, "AAAAAAAAAAA");
-        cars.set(1, "BBBBBBBBBBB");
-        cars.set(1, "BBBBBBBBBBB");
         cars.set(1, "BBBBBBBBBBB");
         for(String p :cars)System.out.println(p);
 
