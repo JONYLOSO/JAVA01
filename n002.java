6471e392d6eb5b15a2e654e243587d82ad2be9b8
JONYLOSO <tana_5678@hotmail.com>
2020-01-01 22:20:43 +0700

    test00000

--- n002.java
+++ n002.java
@@ -1,12 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 class n002 {
     public static void main(String[] args){
-
-        System.out.println("Hello World");
-
-
-
-
-
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> n00 = new ArrayList<Integer>();
+        String cg = new String(); 
+     //  cg = sc.nextLine();
+     
+        for(int i=101;i>0;i--)n00.add(i);  
+      Collections.max(n00); 
+        //n00.add(1);
+        for(int number : n00) System.out.println(number+"\n==============");
+        System.out.println("Hello World :: "+Collections.max(n00)); 
 
 
 
---
+++ n003.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+class n003{
+
+    public static void main(String[] args){
+        HashMap<String,String> nn00 = new  HashMap<String,String>();
+        nn00.put("Eng","London");
+        nn00.put("Th","Bangkok");
+        System.out.println(nn00);
+        System.out.println(nn00.get("Eng"));
+
+
+    }
+
+}
